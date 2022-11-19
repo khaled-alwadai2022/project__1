c2f4762c4e5b54ddb473b02cf751919443e442a2
Sarah <sarah.algodah@outlook.com>
2022-11-19 20:00:55 +0300

    Sarah - 1

--- Book/view_book.dart
+++ Book/view_book.dart
@@ -1,25 +1,21 @@
-import 'dart:io';
 import 'book.dart';
 
 class ViewBookinfoe extends Books {
 
   @override
-  viewBookInformation() {
+  viewBookInformation() { //This Function will view all the information we have about books
      for (int i = 0; i < bookList.length; i++ ) {
       print("${bookList[i]["ID"]}  |  ${bookList[i]["Name"  ]}  |  ${bookList[i]["Author"]}  |  ${bookList[i]["Price"]} | ${bookList[i]["Name"]} | ${bookList[i]["Quantity"]}\n");  
      }
   }
 
-//Abstract Function Class
+
   @override
   addBook(){}
-
   @override
   buyBook() {}
-
   @override
   deleteBook() {}
-
   @override
   editBook() {}
 
--- main.dart
+++ main.dart
@@ -1,103 +1,68 @@
-import 'Book/serch.dart';
 import 'Book/view_book.dart';
 import 'packages.dart';
 
 main(){
-  EditBookInfo editInfo = EditBookInfo();
-  ViewBookinfoe viewInfo = ViewBookinfoe();
-  SellingBooks sellBook = SellingBooks();
-  // AddBook add = AddBook();
-  // AddBook ab = AddBook();
-  DeleteBook deleteBook = DeleteBook();
+  EditBookInfo editInfo = EditBookInfo(); //Object from Edit Calss
+  ViewBookinfoe viewInfo = ViewBookinfoe(); //Object from View Information Class
+  SellingBooks sellBook = SellingBooks(); //Object from View Sell Class
+  DeleteBook deleteBook = DeleteBook(); //Object from Delete Class
 
-
-  var serch = SerchById();
-  var serchBytitle = SerchByTitle();
-  var searchByAuthor = SerchByAthor();
-
- while (true) {
-     print("\n   \x1b[1;33mWlocme To AKS Library ^_^\x1b[0m");
+ while (true) { // Infinity loop
+   print("\n   \x1b[1;33mWlocme To AKS Library ^_^\x1b[0m");
    print("---------------------------------");
    stdout.write("Plase Enter The number To Choose:");
    stdout.write("\n 1.Books Information \n 2.Add New Book \n 3.Edit Book information \n 4.Delete Book \n 5.Search for Book \n 6.Book for sell \n 7.Exit \n---------------------------------\n\x1b[1;34m Enter No: \x1b[0m");
-   String? num = stdin.readLineSync();
+   
+   int no =int.parse(stdin.readLineSync()!);//Read From User
    
    print("---------------------------------\n");
-  switch(int.parse(num!)) { 
-    case 1:{
+  switch(no) { 
+
+    case 1:{ //This Case to View all Books information
       print("Books Information:");
       viewInfo.viewBookInformation();
+      
     } break;
 
     case 2:{
       print("Add Book:");
-      // add.addBook();
-      // add.viewBookInformation();
 
     } break;
 
-    case 3: {
+    case 3: {//In this Case we can EDit on Book Information
       print("Edit Book Informatin:");
       editInfo.editBook();
 
     } break;
 
-    case 4: {
+    case 4: {//To Delete a Book by ID
       print("Delete Book:");
       deleteBook.deleteBook();
       deleteBook.viewBookInformation();
     } break;
 
     case 5: {
-             {
-          while (true) {
-            stdout.write(
-                "\n 1.serch by ID \n 2.serch by Title \n 3.serch by author \n 4.exit  \n---------------------------------\n\x1b[1;34m Enter No: \x1b[0m");
-            String? num1 = stdin.readLineSync();
+      print("Search for Book:");
 
-            switch (int.parse(num1!)) {
-              case 1:
-                print("serach by id");
+    } break;
 
-                serch.SerchId();
-
-                break;
-              case 2:
-                {
-                  print("search by Tatil");
-                  serchBytitle.SerchTitle();
-                }
-                break;
-              case 3:
-                {
-                  print("search by Author");
-                  searchByAuthor.SerchByAuthor();
-                }
-                break;
-             
-            }
-          }
-        }
-
-
-}
-
-    case 6: {
+    case 6: { //In this Case we Will sell the books and see how Quantity we have
       print("Book For Sell:");
       sellBook.buyBook();
       print("\n\x1b[1;37;47mBook Informatin After Selling:\x1b[0m\n");
       sellBook.viewBookInformation();
 
     }break;
-    case 7: {
+    case 7: { // To Exit From the Program
       print(" \x1b[1;32mThank you, Good Bye!\x1b[0m\n");
       exit(0);
     } 
+
+    //If User input user is wrong
     default: { 
     print("\x1b[1;31mWrong choice -_-! Let's try Agin..\x1b[0m");
    }
-  
-} 
+  } 
  }
 }
 
